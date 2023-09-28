a596467de0239b41cb65fdbf60a64dd43d3a6d54
TrevorNathan <trevoratwijukire@gmail.com>
2023-09-29 01:36:18 +0300

    UPDATE: answer fine-tunning

--- unit-1/lesson-2/malacandra.go
+++ unit-1/lesson-2/malacandra.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	// Travel distance of 56,000,000 km in 28 days
-	const distanceTravelled = 56000000 //km 
-	const days = 28 //days
+	const hoursPerDay = 24 //hrs
+	var distance = 56000000 //km 
+	var days = 28 //days
 
-	fmt.Println("How fast is:", distanceTravelled/(days*24), "km/h")
+	fmt.Println("How fast is:", distance/(days*hoursPerDay), "km/h")
 }
