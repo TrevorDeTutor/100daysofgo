9efb2c7bf3f5a8ab16eb9ec8cc4beb713763e75f
TrevorNathan <trevoratwijukire@gmail.com>
2023-09-29 09:48:23 +0300

    ADD: passed calc. leapyear challenge

---
+++ unit-1/lesson-3/leapyear.go
@@ -0,0 +1,17 @@
+// The rules for determining a leap year are as follows:
+// - Any year that is evenly divisible by 4 but not evenly divisible by 100
+// - Or any year that is evenly divisible by 400
+
+package main
+
+import "fmt"
+
+func main() {
+	var year = 2009  //eg. 2008 is a leap year
+
+	if (year%4 == 0 && year%100 != 0) || (year%400 == 0) {
+		fmt.Println(year, " is a leap year")
+	} else {
+		fmt.Println(year, "is not a leap year")
+	}
+}
