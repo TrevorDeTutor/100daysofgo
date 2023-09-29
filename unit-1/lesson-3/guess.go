47257860d33eb96d6a04411ba43f68d8ac91457d
TrevorNathan <trevoratwijukire@gmail.com>
2023-09-29 19:33:14 +0300

    ADD: passed guess-the-number program

---
+++ unit-1/lesson-3/guess.go
@@ -0,0 +1,30 @@
+// Write a guess-the-number program. 
+//Make the computer pick random numbers between 1–100 until it guesses your number, 
+//which you declare at the top of the program. 
+//Display each guess and whether it was too big or too small.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var mynum = 39
+	
+	for {
+		var randomnum = rand.Intn(100) + 1
+		
+		fmt.Println("Random number is: ", randomnum)
+		time.Sleep(time.Second)
+		if randomnum == mynum {
+			fmt.Println("Got the number")
+			break
+		} else {
+			fmt.Println("Let me try again!")
+		}
+	}	
+	
+}
