85903cec8e381e056d24a113d5cac230e269a8db
TrevorNathan <trevoratwijukire@gmail.com>
2023-10-17 16:50:25 +0300

    ADD: experiment challenge qtns

---
+++ unit-2/cipher.go
@@ -0,0 +1,5 @@
+/*
+To send ciphered messages, write a program that ciphers plain text using a keyword:
+plainText := "your message goes here"
+keyword := "GOLANG"
+*/
---
+++ unit-2/decipher.go
@@ -0,0 +1,7 @@
+/*
+Write a program to decipher the ciphered text shown in table 11.2. To keep it
+simple, all characters are uppercase English letters for both the text and keyword:
+
+cipherText := "CSOITEUIWUIZNSROCNKFD"
+keyword := "GOLANG"
+*/
