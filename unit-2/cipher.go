112e33056a326650d07a4045d423384f04e18a73
TrevorNathan <trevoratwijukire@gmail.com>
2023-10-17 19:05:51 +0300

    ADD: experiment solns

--- unit-2/cipher.go
+++ unit-2/cipher.go
@@ -3,3 +3,37 @@ To send ciphered messages, write a program that ciphers plain text using a keywo
 plainText := "your message goes here"
 keyword := "GOLANG"
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	message := "hello from golang"
+	keyword := "golang"
+	keyIndex := 0
+	cipherText := ""
+
+	message = strings.ToUpper(strings.Replace(message, " ", "", -1))
+	keyword = strings.ToUpper(strings.Replace(keyword, " ", "", -1))
+	
+	for i := 0; i < len(message); i++ {
+		c := message[i]
+
+		if c >= 'A' && c <= 'Z' {
+			// A=0, B=1, ... Z=25
+			c -= 'A'
+			k := keyword[keyIndex] - 'A'
+			// cipher letter + key letter
+			c = (c+k)%26 + 'A'
+			// increment keyIndex
+			keyIndex++
+			keyIndex %= len(keyword)
+			}
+			cipherText += string(c)
+		}
+
+		fmt.Println(cipherText)
+}
---
+++ unit-3/README.md
@@ -0,0 +1,4 @@
+## Unit 3. Building blocks
+
+Programming is the breaking of one big impossible task into several very
+small possible tasks. ~ Jazzwant
